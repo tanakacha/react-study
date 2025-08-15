871be80900689d6fcae2a3ecc980e77f056a5c7d
tanakacha <kntasihc20.18@gmail.com>
2025-08-15 21:43:20 +0900

    feat: 条件分岐とリストのレンダー

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-};
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1},
+  { title: 'Garlic', isFruit: false, id: 2},
+  {title: 'Apple', isFruit: true, id: 3},
+  { title: 'Banana', isFruit: true, id: 4},
+  { title: 'Orange', isFruit: true, id: 5},
+  { title: 'Chocolate', isFruit: false, id: 6},
+];
 
-export default function Profile() {
+export default function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'maggenta' : 'darkgreen',
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
   return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }}
-      />
-    </>
+    <ul>{listItems}</ul>
   );
 }
