f615c4d7939ae099bc3fad2c6e72ebe9ea4a52f3
tanakacha <kntasihc20.18@gmail.com>
2025-08-15 21:54:27 +0900

    feat: 独立したstate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1},
-  { title: 'Garlic', isFruit: false, id: 2},
-  {title: 'Apple', isFruit: true, id: 3},
-  { title: 'Banana', isFruit: true, id: 4},
-  { title: 'Orange', isFruit: true, id: 5},
-  { title: 'Chocolate', isFruit: false, id: 6},
-];
+import { useState } from "react";
 
-export default function ShoppingList() {
-  const listItems = products.map(product =>
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'maggenta' : 'darkgreen',
-      }}
-    >
-      {product.title}
-    </li>
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
   );
+}
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
 
   return (
-    <ul>{listItems}</ul>
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
   );
 }
